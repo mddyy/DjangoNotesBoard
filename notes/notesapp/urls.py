bd99b453ea3144c2d906b94be27e994e8059ebf7
mddyy <midday70@yandex.ru>
2023-05-16 21:44:29 +0700

    представление MainPage переделано в класс. остались закомментированные куски кода от примера

--- notes/notesapp/urls.py
+++ notes/notesapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from . import views
+from .views import MainPage
 
 
 urlpatterns = [
-    path('', views.main_page, name='main_page'),
+    path('', MainPage.as_view()),
 ]
--- notes/notesapp/views.py
+++ notes/notesapp/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.views.generic.base import TemplateView
+from .models import Category, Note, Archive
 
 
-def main_page(request):
-    return render(request, 'notesapp/main_page.html', {})
+class MainPage(TemplateView):
+    template_name = 'notesapp/main_page.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(MainPage, self).get_context_data(**kwargs)
+        #notes = Note.objects.all()
+
+        """subjects = set()
+        for note in notes:
+            subject_name = score.subject.name
+            subjects.add(subject_name)
+            student_scores[score.student][subject_name] = score.value
+
+        subjects = sorted(subjects)
+        student_statistics = [
+            {
+                'student': student,
+                'scores': [f'{scores[subject]:.1f}' for subject in subjects]
+            }
+            for student, scores in student_scores.items()
+        ]
+        context.update(
+            {
+                'subjects': subjects,
+                'student_statistics': student_statistics
+            }
+        )"""
+        return context
