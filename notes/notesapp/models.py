346e2c0218a474ec669f404624c546a10862f998
mddyy <midday70@yandex.ru>
2023-05-18 17:47:25 +0700

    модель учитывает пользователя, создавшего заметку

--- notes/notesapp/models.py
+++ notes/notesapp/models.py
@@ -1,5 +1,6 @@
 from datetime import date
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Category(models.Model):
@@ -17,11 +18,14 @@ class Category(models.Model):
 class Note(models.Model):
     title = models.CharField(max_length=50)
     text = models.TextField(blank=True)
-    date_time = models.DateTimeField(null=True)
-    date_only = models.BooleanField(default=True, null=True)
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    username = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
 
 
 class Archive(models.Model):
     note_id = models.ForeignKey(Note, on_delete=models.CASCADE)
     archivation_date = models.DateField(default=date.today())
+
+    @staticmethod
+    def archivate(note: Note):
+        return Archive(note_id = note.id, archivation_date = date.today())
--- notes/notesapp/views.py
+++ notes/notesapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from .models import Category, Note, Archive
+from collections import defaultdict
 
 
 class MainPage(TemplateView):
@@ -8,26 +9,11 @@ class MainPage(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(MainPage, self).get_context_data(**kwargs)
-        #notes = Note.objects.all()
+        notes = Note.objects.all()
 
-        """subjects = set()
-        for note in notes:
-            subject_name = score.subject.name
-            subjects.add(subject_name)
-            student_scores[score.student][subject_name] = score.value
-
-        subjects = sorted(subjects)
-        student_statistics = [
-            {
-                'student': student,
-                'scores': [f'{scores[subject]:.1f}' for subject in subjects]
-            }
-            for student, scores in student_scores.items()
-        ]
         context.update(
             {
-                'subjects': subjects,
-                'student_statistics': student_statistics
+                'notes': notes,
             }
-        )"""
+        )
         return context
