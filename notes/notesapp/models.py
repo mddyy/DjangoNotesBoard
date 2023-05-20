781bf4d7089398e9c1f30bab68f8665056dae980
mddyy <midday70@yandex.ru>
2023-05-21 02:21:58 +0700

    добавлены методы str для моделей

--- notes/notesapp/models.py
+++ notes/notesapp/models.py
@@ -6,6 +6,9 @@ from django.utils import timezone
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    def __str__(self):
+        return self.name
+
 
 class Note(models.Model):
     COLOR_CHOICES = [
@@ -15,6 +18,7 @@ class Note(models.Model):
         ('#66CCFF', 'Blue'),
         ('#FF9933', 'Orange')
     ]
+
     title = models.CharField(max_length=50)
     text = models.TextField(blank=True)
     last_change = models.DateTimeField(default=timezone.now())
@@ -22,11 +26,17 @@ class Note(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     color = models.CharField(max_length=7, choices=COLOR_CHOICES, default=COLOR_CHOICES[0])
 
+    def __str__(self):
+        return self.title
+
 
 class Archive(models.Model):
-    note_id = models.ForeignKey(Note, on_delete=models.CASCADE)
+    note = models.ForeignKey(Note, on_delete=models.CASCADE)
     archivation_date = models.DateField(default=timezone.now().date())
 
     @staticmethod
     def archivate(note: Note):
-        return Archive(note_id=note.id)
+        return Archive(note=note)
+
+    def __str__(self):
+        return str(self.note)
