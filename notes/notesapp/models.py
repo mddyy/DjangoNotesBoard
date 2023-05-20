df314048df976368b9dc4ac5da58dcf42830aa22
mddyy <midday70@yandex.ru>
2023-05-21 03:12:01 +0700

    архивные заметки теперь не отображаются

--- notes/notesapp/models.py
+++ notes/notesapp/models.py
@@ -26,19 +26,19 @@ class Note(models.Model):
     category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True)
     color = models.CharField(max_length=7, choices=COLOR_CHOICES, default=COLOR_CHOICES[0])
 
-    def is_arvivated(self) -> bool:
+    def _is_arvivated(self) -> bool:
         return len(Archive.objects.filter(note=self)) == 1
 
     def __str__(self):
         return self.title
 
     def archivate(self):
-        if not self.is_arvivated():
+        if self._is_arvivated():
             Archive.objects.create(note=self)
 
     def dearchivate(self):
-        if self.is_arvivated():
-            Archive.objects.get(note=self).delete()
+        if self._is_arvivated():
+            Archive.objects.filter(note=self).delete()
 
 
 class Archive(models.Model):
--- notes/notesapp/views.py
+++ notes/notesapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic.base import TemplateView, View
-from .models import Category, Note
+from .models import Category, Note, Archive
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
@@ -9,16 +9,25 @@ class MainPage(LoginRequiredMixin, TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(MainPage, self).get_context_data(**kwargs)
-        categories = (note.category for note in Note.objects.filter(creator=self.request.user))
 
+        # отобрать все заметки данного пользователя за исключением архивированных
+        archive = {item.note for item in Archive.objects.all()}
+        categories = (note.category for note in Note.objects.filter(creator=self.request.user) if note not in archive)
+
+        # сгруппировать заметки по категориям и отсортировать по дате изменения
         notes = [
             {
                 'category': category,
-                'notes_of_category': [note for note in Note.objects.filter(creator=self.request.user, category=category).order_by('last_change')]
+                'notes_of_category': [
+                    note for note in Note.objects\
+                        .filter(creator=self.request.user, category=category)\
+                        .order_by('last_change')
+                ]
             }
             for category in categories
         ]
 
+        # передать полученный список в шаблон
         context.update(
             {
                 'notes': notes,
